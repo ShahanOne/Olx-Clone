b0fd7e10b541746659ba50c29e523e1193aa6192
Shahan.One <shahanhussain9105@gmail.com>
2023-01-11 01:14:10 +0530

    Update Items.jsx

--- client/src/components/Items.jsx
+++ client/src/components/Items.jsx
@@ -4,18 +4,19 @@ import ItemCard from './ItemCard';
 function Items(props) {
   const [itemData, setItemData] = useState('');
 
-  useEffect(() => {
-    async function getItemsData() {
-      await fetch('/api', {
-        mode: 'no-cors',
-      })
-        .then((res) => res.json())
-        .then((data) => setItemData(data));
-    }
+  // useEffect(() => {
+  // async function getItemsData() {
+  // await
+  fetch('/api', {
+    mode: 'no-cors',
+  })
+    .then((res) => res.json())
+    .then((data) => setItemData(data));
+  // }
 
-    getItemsData();
-  }, [props.seed]);
-  console.log(itemData);
+  //   getItemsData();
+  // }, [props.seed]);
+  // console.log(itemData);
   return (
     <div className="itemsDiv">
       {itemData
