5af4fa39aa78cbc4947913886d8e9d81b73cd9fe
Shahan.One <shahanhussain9105@gmail.com>
2023-01-11 01:02:09 +0530

    Update Items.jsx

--- client/src/components/Items.jsx
+++ client/src/components/Items.jsx
@@ -4,33 +4,31 @@ import ItemCard from './ItemCard';
 function Items(props) {
   const [itemData, setItemData] = useState('');
 
-  // useEffect(() => {
-  // async function getItemsData() {
-  // await
-  fetch('/api', {
-    mode: 'no-cors',
-  })
-    .then((res) => res.json())
-    .then((data) => setItemData(data));
-  // }
+  useEffect(() => {
+    async function getItemsData() {
+      await fetch('/api', {
+        mode: 'no-cors',
+      })
+        .then((res) => res.json())
+        .then((data) => setItemData(data));
+    }
 
-  //   getItemsData();
-  // }, []);
-  // console.log(itemData);
+    getItemsData();
+  }, []);
+  console.log(itemData);
   return (
     <div className="itemsDiv">
-      {itemData &&
-        itemData.map((item, index) => (
-          <ItemCard
-            key={index}
-            itemName={item.name}
-            itemPrice={item.price}
-            itemIsSold={item.isSold}
-            notShowBuyBtn={item.isSold}
-            itemImgUrl={item.imageUrl ? item.imageUrl : ''}
-            onBuyClick={() => props.onBuyClick(item)}
-          />
-        ))}
+      {itemData.map((item, index) => (
+        <ItemCard
+          key={index}
+          itemName={item.name}
+          itemPrice={item.price}
+          itemIsSold={item.isSold}
+          notShowBuyBtn={item.isSold}
+          itemImgUrl={item.imageUrl ? item.imageUrl : ''}
+          onBuyClick={() => props.onBuyClick(item)}
+        />
+      ))}
     </div>
   );
 }
